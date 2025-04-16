7ee454e4b7a55271d0eb6db52e82efa859da66c6
sohaihub <soha.r.ihub@snsgroups.com>
2025-04-16 14:46:54 +0530

    Update ChatPage.tsx

--- src/pages/ChatPage.tsx
+++ src/pages/ChatPage.tsx
@@ -5,7 +5,6 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Card } from '@/components/ui/card';
 import { callGeminiAPI } from '@/lib/gemini';
-
 type Message = {
   id: string;
   content: string;
